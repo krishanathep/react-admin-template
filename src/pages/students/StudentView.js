41430a16526fb4344c54bff7d12a62e640fb444c
DESKTOP-JEJQMJU\Krishanathep <krishanathep@gmail.com>
2022-02-14 20:41:05 +0700

    Add Student View Page

--- src/pages/students/StudentView.js
+++ src/pages/students/StudentView.js
@@ -1,7 +1,22 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
 export default function StudentView() {
+  const { id } = useParams();
+  const [detail, setDetail] = useState([]);
+
+  const getData = async () => {
+    await fetch(
+      "https://www.full-stack-app.com/services/public/api/students/" + id
+    )
+      .then((res) => res.json())
+      .then((res) => setDetail(res.student));
+  };
+
+  useEffect(() => {
+    getData(id);
+  }, []);
+
   return (
     <>
       <div className="content-wrapper">
@@ -48,14 +63,39 @@ export default function StudentView() {
                   </div>
                 </div>
                 <div className="card-body">
-                  Start creating your amazing application!
+                  <table className="table table-bordered">
+                    <tr>
+                      <td>ID</td>
+                      <td>{detail.id}</td>
+                    </tr>
+                    <tr>
+                      <td>Name</td>
+                      <td>{detail.name}</td>
+                    </tr>
+                    <tr>
+                      <td>Course</td>
+                      <td>{detail.course}</td>
+                    </tr>
+                    <tr>
+                      <td>Email</td>
+                      <td>{detail.email}</td>
+                    </tr>
+                    <tr>
+                      <td>Phone</td>
+                      <td>{detail.phone}</td>
+                    </tr>
+                  </table>
+                  <div className="form-group mt-2 float-right">
+                  <Link to="/students" className="btn btn-danger">
+                    Cancel
+                  </Link>
+                  </div>
                 </div>
-                <div className="card-footer">Footer</div>
               </div>
             </div>
           </div>
         </section>
       </div>
     </>
-  )
+  );
 }
